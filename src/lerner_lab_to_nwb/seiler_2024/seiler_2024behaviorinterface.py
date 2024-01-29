6cec866d815d312132fa191eb8c686d84eadc6fd
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-01-29 15:42:40 -0800

    extract start and end lines from medpc file

--- src/lerner_lab_to_nwb/seiler_2024/seiler_2024behaviorinterface.py
+++ src/lerner_lab_to_nwb/seiler_2024/seiler_2024behaviorinterface.py
@@ -1,5 +1,6 @@
 """Primary class for converting experiment-specific behavior."""
 from pynwb.file import NWBFile
+import pandas as pd
 
 from neuroconv.basedatainterface import BaseDataInterface
 from neuroconv.utils import DeepDict
@@ -20,5 +21,30 @@ class Seiler2024BehaviorInterface(BaseDataInterface):
         return metadata
 
     def add_to_nwbfile(self, nwbfile: NWBFile, metadata: dict):
-        print(f'{self.source_data["file_path"] = }')
-        print(f'{self.source_data["session_id"] = }')
+        start_date = self.source_data["session_id"].split("_")[1].replace("-", "/")
+        read_medpc_file(file_path=self.source_data["file_path"], start_date=start_date)
+
+
+def read_medpc_file(file_path: str, start_date: str) -> pd.DataFrame:
+    """Read a raw MedPC text file into a pandas DataFrame."""
+    # Read the file
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+
+    # Find the start and end lines for the given session
+    start_line, end_line = None, None
+    for i, line in enumerate(lines):
+        if line == f"Start Date: {start_date}\n":
+            start_line = i
+        elif start_line is not None and line == "\n":
+            end_line = i
+            break
+    if start_line is None:
+        raise ValueError(f"Could not find start date {start_date} in file {file_path}")
+    if end_line is None:
+        raise ValueError(f"Could not find end of session ('\\n') in file {file_path}")
+    session_lines = lines[start_line:end_line]
+    print(start_line, end_line)
+
+    with open("temp.txt", "w") as f:
+        f.writelines(session_lines)
