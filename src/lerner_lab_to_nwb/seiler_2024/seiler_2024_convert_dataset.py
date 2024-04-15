1ecdc86a25d438bbac601ae0229989926ae9292f
pauladkisson <paul.wesley.adkisson@gmail.com>
2024-04-15 12:44:21 -0700

    opto_to_nwb initial setup

--- src/lerner_lab_to_nwb/seiler_2024/seiler_2024_convert_dataset.py
+++ src/lerner_lab_to_nwb/seiler_2024/seiler_2024_convert_dataset.py
@@ -19,16 +19,22 @@ def dataset_to_nwb(
     start_variable = "Start Date"
     data_dir_path = Path(data_dir_path)
     output_dir_path = Path(output_dir_path)
-    session_to_nwb_args_per_session = fp_to_nwb(
+    # session_to_nwb_args_per_session = fp_to_nwb(
+    #     data_dir_path=data_dir_path,
+    #     output_dir_path=output_dir_path,
+    #     start_variable=start_variable,
+    #     stub_test=stub_test,
+    #     verbose=verbose,
+    # )
+
+    session_to_nwb_args_per_session = opto_to_nwb(
         data_dir_path=data_dir_path,
         output_dir_path=output_dir_path,
-        start_variable=start_variable,
         stub_test=stub_test,
         verbose=verbose,
     )
 
     # Convert all sessions and handle missing Fi1d's
-    return
     missing_fi1d_sessions = []
     for session_to_nwb_args in tqdm(session_to_nwb_args_per_session):
         try:
@@ -217,6 +223,115 @@ def fp_to_nwb(
                 nwbfile_paths.add(nwbfile_path)
                 session_to_nwb_args_per_session.append(session_to_nwb_args)
     return session_to_nwb_args_per_session
+
+
+def opto_to_nwb(
+    data_dir_path: Union[str, Path], output_dir_path: Union[str, Path], stub_test: bool = False, verbose: bool = True
+):
+    experiment_type = "Opto"
+    experimental_groups = ["DLS-Excitatory", "DMS-Excitatory", "DMS-Inhibitory"]
+    experimental_group_to_optogenetic_treatments = {
+        "DLS-Excitatory": ["ChR2", "EYFP", "ChR2Scrambled"],
+        "DMS-Excitatory": ["ChR2", "EYFP", "ChR2Scrambled"],
+        "DMS-Inhibitory": ["NpHR", "EYFP", "NpHRScrambled"],
+    }
+    optogenetic_treatment_to_folder_name = {
+        "ChR2": "ChR2",
+        "EYFP": "EYFP",
+        "ChR2Scrambled": "Scrambled",
+        "NpHR": "Halo",
+        "NpHRScrambled": "Scrambled",
+    }
+    experimental_group_to_subgroups = {
+        "DLS-Excitatory": [""],
+        "DMS-Excitatory": [""],
+        "DMS-Inhibitory": ["Group 1"],  # TODO: Get group 2 data from Lerner Lab
+    }
+    opto_path = data_dir_path / f"{experiment_type} Experiments"
+    start_variable = "Start Date"
+    session_to_nwb_args_per_session: list[dict] = []  # Each dict contains the args for session_to_nwb for a session
+    nwbfile_paths = set()  # Each path is the path to the nwb file created for a session
+
+    for experimental_group in experimental_groups:
+        experimental_group_path = opto_path / experimental_group.replace("-", " ")
+        for subgroup in experimental_group_to_subgroups[experimental_group]:
+            subgroup_path = experimental_group_path / subgroup if subgroup else experimental_group_path
+            optogenetic_treatments = experimental_group_to_optogenetic_treatments[experimental_group]
+            for optogenetic_treatment in optogenetic_treatments:
+                optogenetic_treatment_path = subgroup_path / optogenetic_treatment_to_folder_name[optogenetic_treatment]
+                subject_paths = [
+                    path
+                    for path in optogenetic_treatment_path.iterdir()
+                    if not (
+                        path.name.startswith(".")
+                        or path.name.endswith(".csv")
+                        or path.name.endswith(".CSV")  # TODO: add support for CSV files
+                    )
+                ]
+                for subject_path in subject_paths:
+                    subject_id = subject_path.name
+                    header_variables = get_opto_header_variables(subject_path, start_variable)
+                    start_dates, start_times, msns, file_paths, subjects, box_numbers = header_variables
+                    for start_date, start_time, msn, file, subject, box_number in zip(
+                        start_dates, start_times, msns, file_paths, subjects, box_numbers
+                    ):
+                        if session_should_be_skipped(
+                            start_date=start_date,
+                            start_time=start_time,
+                            subject_id=subject_id,
+                            msn=msn,
+                        ):
+                            continue
+                    session_conditions = {
+                        "Start Date": start_date,
+                        "Start Time": start_time,
+                    }
+                    if subject is not None:
+                        session_conditions["Subject"] = subject
+                    if box_number is not None:
+                        session_conditions["Box"] = box_number
+                    start_datetime = datetime.strptime(f"{start_date} {start_time}", "%m/%d/%y %H:%M:%S")
+                    session_to_nwb_args = dict(
+                        data_dir_path=data_dir_path,
+                        output_dir_path=output_dir_path,
+                        behavior_file_path=file,
+                        subject_id=subject_id,
+                        session_conditions=session_conditions,
+                        start_variable=start_variable,
+                        start_datetime=start_datetime,
+                        experiment_type=experiment_type,
+                        experimental_group=experimental_group,
+                        optogenetic_treatment=optogenetic_treatment,
+                        stub_test=stub_test,
+                        verbose=verbose,
+                    )
+                    nwbfile_path = (
+                        output_dir_path
+                        / f"{experiment_type}_{experimental_group}__{optogenetic_treatment}_{subject_id}_{start_datetime.isoformat()}.nwb"
+                    )
+                    if nwbfile_path in nwbfile_paths:
+                        continue
+                    nwbfile_paths.add(nwbfile_path)
+                    session_to_nwb_args_per_session.append(session_to_nwb_args)
+    return session_to_nwb_args_per_session
+
+
+def get_opto_header_variables(subject_path, start_variable):
+    if subject_path.is_file():
+        medpc_file_path = subject_path
+        medpc_variables = get_medpc_variables(
+            file_path=medpc_file_path, variable_names=["Start Date", "Start Time", "MSN"]
+        )
+        start_dates = medpc_variables["Start Date"]
+        start_times = medpc_variables["Start Time"]
+        msns = medpc_variables["MSN"]
+        file_paths = [medpc_file_path] * len(start_dates)
+        subjects = [None] * len(start_dates)
+        box_numbers = [None] * len(start_dates)
+    else:
+        return [], [], [], [], [], []
+
+    return start_dates, start_times, msns, file_paths, subjects, box_numbers
 
 
 def session_should_be_skipped(*, start_date, start_time, subject_id, msn):
